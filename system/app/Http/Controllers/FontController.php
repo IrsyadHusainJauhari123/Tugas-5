bf5368e490d0c471f8aabe309d075edf3a341f2b
IrsyadHusainJauhari123 <108142445+IrsyadHusainJauhari123@users.noreply.github.com>
2022-08-29 23:24:11 +0700

    Initial commit

---
+++ system/app/Http/Controllers/FontController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FontController extends Controller
+{
+    function showabout()
+    {
+        return view('fontend.about');
+    }
+
+    function showlogin()
+    {
+        return view('fontend.login');
+    }
+
+
+    function showcart()
+    {
+        return view('fontend.cart');
+    }
+
+
+    function showcheckout()
+    {
+        return view('fontend.checkout');
+    }
+
+
+    function showsingle()
+    {
+        return view('fontend.single');
+    }
+
+    function showcontact()
+    {
+        return view('fontend.contact');
+    }
+
+
+    function showhome()
+    {
+        return view('fontend.home');
+    }
+
+    function showshop()
+    {
+        return view('fontend.shop');
+    }
+}
